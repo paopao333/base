473e0dc76d3f231c307db9f5b96865a06f2bae22
gaiting.feng <gaiting.feng@pcitc.com>
2021-08-04 18:13:07 +0800

    编译

---
+++ src/utils/index.js
@@ -0,0 +1,12 @@
+import dayjs from 'dayjs'
+const plugins = { day: dayjs }
+export default {
+  install(Vue) {
+    // 挂载插件
+    Object.keys(plugins).forEach(key => {
+      Vue.$prototype['$' + key] = plugins[key]
+    })
+    // 挂载utils
+
+  }
+}
